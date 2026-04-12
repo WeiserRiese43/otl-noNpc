7b8eb5ff2bf213a428f864bd952b417aa2148c2f
WeiserRiese43 <nicobraun4411@gmail.com>
2026-04-12 18:46:35 +0200

    Made some performance fixes, no train spawn anymore

--- client.lua
+++ client.lua
@@ -1,28 +1,34 @@
 Citizen.CreateThread(function()
-    while true do
-    Citizen.Wait(0)
-
-    -- Set Pedestrians and Vehicle Density to 0
-    SetPedDensityMultiplierThisFrame(0.0)
-    SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
-    SetVehicleDensityMultiplierThisFrame(0.0)
-    SetRandomVehicleDensityMultiplierThisFrame(0.0)
-    SetParkedVehicleDensityMultiplierThisFrame(0.0)
-
-    -- Disable specific scenarios that spawn NPCs
     SetGarbageTrucks(false)
     SetRandomBoats(false)
     SetCreateRandomCops(false)
     SetCreateRandomCopsNotOnScenarios(false)
     SetCreateRandomCopsOnScenarios(false)
-
-    -- Delete generated parked Vehicles nearby
-    local playerPed = PlayerPedId()
-    local playerCoords = GetEntityCoords(playerPed)
-    RemoveVehiclesFromGeneratorsInArea(playerCoords.x - 500.0, playerCoords.y - 500.0, playerCoords.z - 500.0, playerCoords.x + 500.0, playerCoords.y + 500.0, playerCoords.z + 500.0)
+    SetRandomTrains(false)
 
     for i = 1, 15 do
         EnableDispatchService(i, false)
     end
-end
+
+    while true do
+        Citizen.Wait(0)
+        SetVehicleDensityMultiplierThisFrame(0.0)
+        SetPedDensityMultiplierThisFrame(0.0)
+        SetRandomVehicleDensityMultiplierThisFrame(0.0)
+        SetParkedVehicleDensityMultiplierThisFrame(0.0)
+        SetScenarioPedDensityMultiplierThisFrame(0.0, 0.0)
+    end
+end)
+
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(1000)
+        local playerPed = PlayerPedId()
+        local playerCoords = GetEntityCoords(playerPed)
+
+        RemoveVehiclesFromGeneratorsInArea(
+        playerCoords.x - 500.0, playerCoords.y - 500.0, playerCoords.z - 500.0,
+        playerCoords.x + 500.0, playerCoords.y + 500.0, playerCoords.z + 500.0
+        )
+    end
 end)
